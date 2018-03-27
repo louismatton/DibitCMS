0ee638b7405ac0b2e3eaaff5c81f9ebaec00f0bf
Louis Matton <louis.matton@outlook.com>
2018-03-27 12:39:34 +0200

    navigatie css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  isSelected(route): boolean {
+    // var lis = document.getElementsByTagName("nav")[0].getElementsByTagName("a");
+    // console.log(lis);
+    var paramValue = window.location.href.split("/");
+    if (paramValue[3] == route) {
+      return true;
+    }
+    if (paramValue[3] + paramValue[4] == route) {
+      return true;
+    }
+    return false;
+  }
+
+  navigationItems: any[] = [{
+      image: "fas fa-tachometer-alt fa-2x",
+      name: "Dashboard",
+      routerLink: "dashboard"
+    },
+    {
+      image: "far fa-newspaper fa-2x",
+      name: "Pages",
+      routerLink: "pages"
+    },
+    {
+      image: "fas fa-users fa-2x",
+      name: "Users",
+      routerLink: "users"
+    },
+    {
+      image: "fas fa-cogs fa-2x",
+      name: "Settings",
+      routerLink: "settings"
+    },
+    {
+      image: "fas fa-sign-out-alt fa-2x",
+      name: "Signout",
+      routerLink: "signout"
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
+import { UsersComponent } from './users/users.component';
+import { SettingsComponent } from './settings/settings.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PagesComponent,
+    UsersComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'pages', component: PagesComponent},
+      {path: 'users', component: UsersComponent},
+      {path: 'settings', component: SettingsComponent},
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+      {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
